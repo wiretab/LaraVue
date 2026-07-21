02c5d0afdca6ef877c40a67d5514446500590071
tabcodes <tabcodes@tabcodes.dev>
2026-07-21 00:09:43 -0400

    Add HasFactory trait

--- app/Models/Provider.php
+++ app/Models/Provider.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasManyThrough;
@@ -10,6 +11,8 @@ use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 #[Fillable(['email', 'name'])]
 class Provider extends Model
 {
+    use HasFactory;
+    
     public function appointments(): HasMany {
         return $this->hasMany(Appointment::class);
     }
