3f8c71ca0bd0f2efc64e23477144a624f4eeacd0
tabcodes <tabcodes@tabcodes.dev>
2026-07-21 08:26:10 -0400

    Add patients FK to appointments (correctly this time)

--- database/migrations/2026_07_16_031941_create_appointments_table.php
+++ database/migrations/2026_07_16_031941_create_appointments_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('appointments', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('patient_id')->constrained()->deleteOnCascade();
             $table->foreignId('provider_id')->constrained()->deleteOnCascade();
             $table->foreignId('appointment_type_id')->constrained()->cascadeOnDelete();
 
